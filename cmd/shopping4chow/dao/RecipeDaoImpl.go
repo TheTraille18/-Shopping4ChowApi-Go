27006e7665fa2a16800aa89c21792a263a33c9c4
TheTraille18 <jtraille07@gmail.com>
2021-09-08 16:33:49 -0400

    AddRecipe functionality added

--- cmd/shopping4chow/dao/RecipeDaoImpl.go
+++ cmd/shopping4chow/dao/RecipeDaoImpl.go
@@ -1,8 +1,11 @@
 package dao
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"shopping4chow/cmd/shopping4chow/models"
+	config "shopping4chow/configs"
 )
 
 type RecipeDaoImpl struct {
@@ -22,7 +25,13 @@ func (r RecipeDaoImpl) GetAllRecipes() []models.Recipe {
 	return nil
 }
 func (r RecipeDaoImpl) AddRecipe(recipe models.Recipe) {
+	log.Println("Adding recipe")
 	fmt.Println(recipe)
-	//config.Conn.QueryRow(context.Background(), "insert into recipe (amount, name, units, ingredient_id, meal_id) values ($1,$2,$3,$4,$5)",
+
+	_, err := config.Conn.Exec(context.Background(), "insert into recipe (amount, name, units, ingredient_id, meal_id) values ($1,$2,$3,$4,$5)", recipe.Amount,
+		recipe.Name, recipe.Units, recipe.Ingredient_id, recipe.Meal_id)
+	if err != nil {
+		fmt.Println(err)
+	}
 	//	recipe.Amount, recipe.Name, recipe.Units, recipe.Ingredient_id, recipe.Meal_id)
 }
