1f0d4017d82171555f0c380758f90e5538304b73
TheTraille18 <jtraille07@gmail.com>
2021-09-11 14:11:04 -0400

    Added unitMap

--- cmd/shopping4chow/models/Recipe.go
+++ cmd/shopping4chow/models/Recipe.go
@@ -2,34 +2,35 @@ package models
 
 type Unit int
 
-const (
-	None Unit = iota
-	Each
-	Peice
-	Bag
-	Bottle
-	Box
-	Pack
-	Jar
-	Can
-	Bunch
-	Roll
-	Dozen
-	Small
-	Large
-	Lbs
-	Qt
-	Oz
-	Cup
-	Dallon
-	Tbsp
-	Tsp
-	G
-	Kg
-	Liter
-	Milliliter
-	Pis
-)
+var unitMap = map[int]string{
+	0:  "none",
+	1:  "each",
+	2:  "peice",
+	3:  "bag",
+	4:  "bottle",
+	5:  "box",
+	6:  "case",
+	7:  "pack",
+	8:  "jar",
+	9:  "can",
+	10: "bunch",
+	11: "roll",
+	12: "dozen",
+	13: "small",
+	14: "large",
+	15: "lbs",
+	16: "qt",
+	17: "oz",
+	18: "cup",
+	19: "gallon",
+	20: "tbsp",
+	21: "tsp",
+	22: "g",
+	23: "kg",
+	24: "liter",
+	25: "milliliter",
+	26: "pis",
+}
 
 type Recipe struct {
 	Id            int    `json:"id"`
@@ -37,5 +38,10 @@ type Recipe struct {
 	Name          string `json:"name"`
 	Ingredient_id int    `json:"ingredientId"`
 	Amount        int    `json:"amount"`
-	Units         Unit   `json:"unit"`
+	UnitInt       int    `json:"unit"`
+	Units         string
+}
+
+func (recipe *Recipe) SetUnits() {
+	recipe.Units = unitMap[recipe.UnitInt]
 }
